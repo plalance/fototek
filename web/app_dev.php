171e16383af20f8e78d91218af5388f10a674ea1
plalance <lalance.paul@gmail.com>
2017-05-12 12:15:39 +0200

    MAJ

--- web/app_dev.php
+++ web/app_dev.php
@@ -23,7 +23,7 @@ use Symfony\Component\Debug\Debug;
 $loader = require __DIR__.'/../app/autoload.php';
 Debug::enable();
 
-$kernel = new AppKernel('dev', true);
+$kernel = new AppKernel('prod', true);
 $kernel->loadClassCache();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
